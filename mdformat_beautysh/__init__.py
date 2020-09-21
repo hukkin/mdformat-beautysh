9df943eeddc348c7b9b6539cdb4a5505830415a6
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-09-21 04:47:46 +0300

    Bump version: 0.0.0 → 0.1.0

--- mdformat_beautysh/__init__.py
+++ mdformat_beautysh/__init__.py
@@ -1,6 +1,6 @@
 from beautysh import Beautify
 
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 
 def format_bash(unformatted: str) -> str:
