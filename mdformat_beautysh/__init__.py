5078da9c2a9a9fe01b6373a8813ef5e558e85b57
Taneli Hukkinen <hukkinen@eurecom.fr>
2025-11-03 11:14:03 +0200

    Bump version: 0.1.1 → 1.0.0

--- mdformat_beautysh/__init__.py
+++ mdformat_beautysh/__init__.py
@@ -1,6 +1,6 @@
 from beautysh import BashFormatter
 
-__version__ = "0.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "1.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 formatter = BashFormatter()
 
